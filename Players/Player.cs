7a2dc6f7e760605bcbb410c1b73918a75f8b8c5e
xNemix <46863616+xNemix@users.noreply.github.com>
2023-01-16 15:36:18 +0100

    Ferdig

---
+++ Players/Player.cs
@@ -0,0 +1,28 @@
+﻿namespace Players
+{
+    public class Player {
+        private int _points;
+        private string _name;
+        private Random _random;
+
+        public Player(string name, int points, Random random)
+        {
+            _name = name;
+            _points = points;
+            _random = random;
+        }
+
+        public void Play(Player player)
+        {
+            var getRandomWinner = _random.Next(2) == 0;
+            var givePointsToWinner = getRandomWinner == true ? _points++ : player._points++;
+            var takePointsFromLoser = getRandomWinner == false ? _points-- : player._points--;
+        }
+
+        public void ShowNameAndPoints()
+        {
+            var outputText = $"Spiller: {_name} har {_points} poeng!";
+            Console.WriteLine(outputText);
+        }
+    }
+}
---
+++ Players/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+namespace Players
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var players = new[] { new Player("Per", 10, random), new Player("Pål", 10, random), new Player("Espen", 10, random) };
+            for (var round = 1; round <= 10; round++)
+            {
+                var playerIndex1 = random.Next(players.Length);
+                var playerIndex2 = (playerIndex1 + 1 + random.Next(2)) % players.Length;
+                var player1 = players[playerIndex1];
+                var player2 = players[playerIndex2];
+                player1.Play(player2);
+            }
+
+            foreach (var player in players)
+            {
+                player.ShowNameAndPoints();
+            }
+        }
+    }
+}
